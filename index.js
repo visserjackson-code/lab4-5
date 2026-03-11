3102a94c8a8eec587dd0bf6a1b0e8e42cd55976f
visserjackson-code <jcv74@proton.me>
2026-03-11 06:06:47 -0400

    news renders dynamically (5.1)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let news = [
+let startingNews = [
 { id: 1, title: 'Election Results',
 content: "Republican Clay Fuller and Democrat Shawn Harris advanced from a crowded field to a runoff in the special election to replace former Rep. Marjorie Taylor Greene in Georgia, NBC News projects." },
 { id: 2, title: 'Sporting Success',
@@ -8,12 +8,21 @@ content: "Thunderstorms, gusty winds possible as Wednesday declared a Severe Wea
 ];
 
 
+function renderNewsItem(newsItem) {
+    const template = document.getElementById('news-item-template').content.cloneNode(true);
+    template.querySelector(".card-title").innerText = newsItem.title;
+    template.querySelector(".card-text").innerText = newsItem.content;
+    document.querySelector("#news-list").appendChild(template);
+
+
+}
+
 const getNewsList = () => {
     return new Promise(resolve => {
         setTimeout(() => {
-            resolve(news);
+            resolve(startingNews);
         }, 5000);
     })
 }
 
-getNewsList().then((newsList) => console.log(newsList));
+getNewsList().then((newsList) => newsList.forEach(n => renderNewsItem(n)));
